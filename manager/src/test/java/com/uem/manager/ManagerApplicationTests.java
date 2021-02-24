48bddffeec3f768ce5133029c34fcda7f84a3835
fersilverio <ssilveriofernando45@gmail.com>
2021-02-24 15:13:13 -0300

    Inicializando estrutura de projeto

---
+++ manager/src/test/java/com/uem/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uem.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
