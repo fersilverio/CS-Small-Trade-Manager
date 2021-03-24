41f1c1ff2c2126be255ce37d882185b72cd5e0e2
fersilverio <ssilveriofernando45@gmail.com>
2021-03-23 22:14:37 -0300

    Alterando estrutura (ate o momento) do produto

--- src/main/java/com/uem/simple/manager/controller/ProductController.java
+++ src/main/java/com/uem/simple/manager/controller/ProductController.java
@@ -2,62 +2,96 @@ package com.uem.simple.manager.controller;
 
 import java.util.List;
 
-import com.uem.simple.manager.model.Produto;
-import com.uem.simple.manager.service.ProdutoService;
+import javax.validation.Valid;
 
+import com.uem.simple.manager.model.Fornecedor;
+import com.uem.simple.manager.model.Produto;
+import com.uem.simple.manager.repository.ProductRepository;
+import com.uem.simple.manager.repository.SupplierRepository;
+import com.uem.simple.manager.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
-    @Autowired
-    private ProdutoService produtoService;
+    private final ProductService productService;
+    private final ProductRepository productRepository;
+    private final SupplierRepository supplierRepository;
 
-    // Listagem de produtos
+    
+    @Autowired
+    public ProductController (ProductService productService, SupplierRepository supplierRepository, ProductRepository productRepository){
+        this.productService = productService;
+        this.productRepository = productRepository;
+        this.supplierRepository = supplierRepository;
+    }
+    
+    
+    
     @GetMapping
-    public String viewProdutoMainPage(Model model, @Param("nome") String nome) {
-        List<Produto> listaProdutos = produtoService.getAllProductsByNome(nome);
-        model.addAttribute("productList", listaProdutos);
-        model.addAttribute("nome", nome);
+    public String product (Model m){
+        m.addAttribute("ListaProdutos", productService.findAll());
         return "product/home";
     }
 
-    // CREATE
+
     @GetMapping("/new")
-    public String showNewProductForm(Model model) {
-        Produto produto = new Produto();
-        model.addAttribute("produto", produto);
+    public String addProduct(Model m){
+        Produto product = new Produto();
+        m.addAttribute("produto", product);
         return "product/new";
     }
 
+
     @PostMapping("/add")
-    public String saveProduct(@ModelAttribute("produto") Produto produto) {
-        produtoService.saveProduct(produto);
-        return "redirect:/product";
+    public String saveProduct(@Valid Produto product, BindingResult br, RedirectAttributes ra){
+        if (br.hasErrors()){
+            return "product/new";
+        }
+        
+        // acessar a lista de fornecedores por nome
+
+        return "";
+        
+
+        //continua
     }
 
-    // UPDATE
-    @GetMapping("/update/{id}")
-    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
-        Produto produto = produtoService.getProductById(id);
-        model.addAttribute("produto", produto);
+
+    @GetMapping("/view/{id}")
+    public String viewProduct(@PathVariable Long id, Model m){
+        Produto product = productService.getProductById(id);
+        m.addAttribute("produto", product);
+        m.addAttribute("update", false);
         return "product/manage";
     }
 
 
-    @GetMapping("/delete/{id}")
-    public String deleteProductById(@PathVariable(value = "id") long id, Model model) {
-        this.produtoService.deleteProductById(id);
-        return "redirect:/product";
+    @GetMapping("/edit/{id}")
+    public String editProduct(@PathVariable Long id, Model m) {
+        Produto product = productService.getProductById(id);
+        m.addAttribute("produto", product);
+        m.addAttribute("update", true);
+        return "supplier/manage";
     }
 
+
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        Produto product = productService.getProductById(id);
+        productRepository.delete(product);
+        return "redirect:/product";
+    }
 
 }
--- src/main/java/com/uem/simple/manager/repository/ProductRepository.java
+++ src/main/java/com/uem/simple/manager/repository/ProductRepository.java
@@ -1,16 +1,8 @@
 package com.uem.simple.manager.repository;
-
-import java.util.List;
-
 import com.uem.simple.manager.model.Produto;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Produto, Long>{
-    
-    @Query("select a from Produto a where a.nome like %?1%")
-    List<Produto> getAllByNome(String nome);
 }
